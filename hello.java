dba2a8157386eccdfd9617901ec1200fe6bd8418
Gaurav Koley <arkokoley@live.in>
2018-01-19 15:01:07 +0530

    java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
+
